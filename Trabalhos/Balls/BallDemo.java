10ae00a2b8e2776c4e767922c4d5042dc22f797c
Rayan Avelino <rayanodb@gmail.com>
2017-08-16 19:31:43 -0300

    Balls <v2.1>

--- Trabalhos/Balls/BallDemo.java
+++ Trabalhos/Balls/BallDemo.java
@@ -12,6 +12,7 @@ import java.util.*;
 public class BallDemo{
 
     private Canvas myCanvas;
+    private ArrayList<BouncingBall> balls;
     private static final int WIDTH = 600;
     private static final int HEIGHT = 500;
 
@@ -23,6 +24,7 @@ public class BallDemo{
 
         myCanvas = new Canvas("Ball Demo", WIDTH, HEIGHT);
         myCanvas.setVisible(true);
+        balls = new ArrayList<BouncingBall>();
     }
 
  
@@ -31,26 +33,28 @@ public class BallDemo{
      */
     public void bounce(int numberOfBalls){
 
+
+        Random rand = new Random();
+        Dimension size = myCanvas.getSize();
+        int h = size.height;
+        int w = size.width;
+        
+        int ground = h - 35;   // position of the ground line
+        int xStart = 50;       // x-start of the ground line
+        int xLimit = w - 50;   // x-limit of the ground line
+
+        myCanvas.setVisible(true);
+
+        // Draw the ground
+        myCanvas.setForegroundColor(Color.blue);
+        myCanvas.drawLine(xStart, ground, xLimit, ground);
+
         if(numberOfBalls < 0){
 
             System.out.println("Número inválido de bolas!");
+            return;
 
         }else{
-
-            Random rand = new Random();
-            Dimension size = myCanvas.getSize();
-            int h = size.height;
-            int w = size.width;
-            
-            int ground = h - 35;   // position of the ground line
-            int xStart = 50;       // x-start of the ground line
-            int xLimit = w - 50;   // x-limit of the ground line
-
-            myCanvas.setVisible(true);
-
-            // Draw the ground
-            myCanvas.setForegroundColor(Color.blue);
-            myCanvas.drawLine(xStart, ground, xLimit, ground);
 
             // Crate and show the balls
             for(int i = 0; i < numberOfBalls; i++){
@@ -64,23 +68,35 @@ public class BallDemo{
                 float g = rand.nextFloat();
                 float b = rand.nextFloat();
                 Color color = new Color(r, g, b);
-                // Ball
-                BouncingBall ball = new BouncingBall(xPos, yPos, 20, color, ground, myCanvas);
-                ball.draw();
 
-                // Make them bounce until both have gone beyond the xLimit.
-                boolean finished =  false;
-                while(!finished) {
-                    myCanvas.wait(50);           // Small delay
-                    ball.move();
-                    // Stop once ball has travelled a certain distance on x axis
-                    if(ball.getXPosition() >= xLimit) {
-                        finished = true;
-                    }
-                }
-                ball.erase();
+                // Random diameter
+                int diameter = rand.nextInt(40) + 10;
+
+                // Ball
+                BouncingBall ball = new BouncingBall(xPos, yPos, diameter, color, ground, myCanvas);
+                balls.add(ball);
+
             }
 
+        }
+
+        // Drawing the balls
+        for(BouncingBall b : balls){
+            b.draw();
+        }
+
+        // Make them bounce until both have gone beyond the xLimit.
+        boolean finished =  false;
+        while(!finished){
+            myCanvas.wait(50);           // Small delay
+            for(BouncingBall b : balls){
+                b.move();
+                // Stop once ball has travelled a certain distance on x axis
+                if(balls.indexOf(b) == balls.size() && b.getXPosition() >= xLimit) {
+                    finished = true;
+                    b.erase();
+                }
+            }
         }
 
     }
@@ -88,9 +104,11 @@ public class BallDemo{
     public void drawFrame(){
 
         myCanvas.erase();
+
         Dimension size = myCanvas.getSize();
         int h = size.height;
         int w = size.width;
+
         Rectangle rect = new Rectangle(20, 20, w - 40, h - 40);
         myCanvas.draw(rect);
 
